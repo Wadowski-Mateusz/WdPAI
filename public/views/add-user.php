a55bf38393821637b42e220d8747ee6b700439d2
Wadowski-Mateusz <mateusz.wadow@gmail.com>
2023-02-14 02:19:56 +0100

    user-add style

--- public/views/add-user.php
+++ public/views/add-user.php
@@ -9,34 +9,27 @@
 <body>
 <div class="base-container">
     <main>
-        <header>
-            <div class="add-user">
-                <i class="fas fa-plus"></i> Add user
-            </div>
-        </header>
+        Dodawanie użytkownika
         <section class="user-form">
             <form action="addUser" method="POST" ENCTYPE="multipart/form-data">
-                <div class="messages">
-                    <?php
-                    if(isset($messages)){
-                        foreach($messages as $message) {
-                            echo $message;
-                        }
-                    }
-                    ?>
-                </div>
+
                 <input name="name" type="text" placeholder="imie">
                 <input name="surname" type="text" placeholder="nazwisko">
-                <input name="pesel" type="text" placeholder="pesel"> <!---wlasciwe do logowania-->
+                <input name="pesel" type="text" placeholder="pesel">
 
-                <label for="roles">Rola użytkownika</label>
                 <select id="role" name="roles">
                     <option value="student">Uczeń</option>
                     <option value="teacher">Nauczyciel</option>
                     <option value="admin">Admin</option>
                 </select>
 
-                <input type="file" name="file"/><br/> <!--zdjecie usera-->
+                <div class="avatar">
+                    <input class="box" type="checkbox" name="isAvatar"/> <!--TODO avatar odblokowany po zaznaczeniu -->
+                    Czy dodać avatar?
+                </div>
+
+                <input class="fileButton" type="file" name="file" placeholder/>
+
 <!--                <textarea name="description" rows=5 placeholder="description"></textarea>--> <!-- pole tekstowe, przyda się do komentowania ocen, wysyłania wiadomości-->
                 <button type="submit">send</button>
 
